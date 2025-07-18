b4b6606be34431792bb712d76fdd3ce0b4f538cb
AJINKYA <ajinkyacodes@gmail.com>
2025-07-18 08:35:20 +0530

    forgot password text and login button added on login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { s, vs } from 'react-native-size-matters';
 import LoginInput from '../components/LoginInput';
@@ -8,6 +8,10 @@ const LoginScreen = () => {
 		<View style={styles.container}>
 			<Text style={styles.welcomeText}>Welcome Back!</Text>
             <LoginInput />
+            <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+            <TouchableOpacity style={styles.loginButton}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>            
 		</View>
 	);
 };
@@ -25,4 +29,22 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 		color: '#000',
 	},
+    forgotPasswordText: {
+        color: '#F83758',
+        fontSize: s(15),
+        marginTop: vs(9),
+        textAlign: 'right'
+    },
+    loginButton: {
+        marginTop: vs(40),
+        backgroundColor: '#F83758',
+        paddingVertical: vs(10),
+        borderRadius: s(4)
+    },
+    loginText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: s(20),
+        fontWeight: 'semibold'
+    }
 });
