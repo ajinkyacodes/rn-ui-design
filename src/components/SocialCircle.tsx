d18c3309b2840e200dd62b490b8b083517305529
AJINKYA <ajinkyacodes@gmail.com>
2025-07-18 10:50:02 +0530

    completed LoginScreen

--- src/components/SocialCircle.tsx
+++ src/components/SocialCircle.tsx
@@ -1,23 +1,27 @@
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import React, { FC } from 'react';
 import { s } from 'react-native-size-matters';
 import { SendIcon } from '../assets/Icons';
 
-const SocialCircle = () => {
-	return <View style={styles.circle}></View>;
+interface SocialCircleProps {
+	icon: React.ReactNode;
+}
+
+const SocialCircle: FC<SocialCircleProps> = ({ icon }) => {
+	return <TouchableOpacity style={styles.circle}>{icon}</TouchableOpacity>;
 };
 
 export default SocialCircle;
 
 const styles = StyleSheet.create({
 	circle: {
-		height: s(46),
-		width: s(46),
+		height: s(54),
+		width: s(54),
 		borderRadius: s(40),
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#fff',
+		backgroundColor: '#FCF3F6',
 		borderWidth: s(1),
-		borderColor: '#E4E6E8',
+		borderColor: '#F83758',
 	},
 });
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,16 +2,63 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { s, vs } from 'react-native-size-matters';
 import LoginInput from '../components/LoginInput';
+import SocialCircle from '../components/SocialCircle';
+import GoogleLogo from '../assets/GoogleLogo';
+import AppleLogo from '../assets/AppleLogo';
+import FacebookLogo from '../assets/FacebookLogo';
 
 const LoginScreen = () => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.welcomeText}>Welcome Back!</Text>
-            <LoginInput />
-            <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
-            <TouchableOpacity style={styles.loginButton}>
-                <Text style={styles.loginText}>Login</Text>
-            </TouchableOpacity>            
+			<LoginInput />
+			<TouchableOpacity>
+				<Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.loginButton}>
+				<Text style={styles.loginText}>Login</Text>
+			</TouchableOpacity>
+			<View style={styles.socialContainer}>
+				<Text style={{ color: 'grey', fontSize: s(12) }}>
+					- OR Continue with -
+				</Text>
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						marginTop: vs(20),
+						marginBottom: vs(28),
+					}}
+				>
+					<SocialCircle icon={<GoogleLogo />} />
+					<View style={{ marginHorizontal: s(10) }}>
+						<SocialCircle icon={<AppleLogo />} />
+					</View>
+					<SocialCircle icon={<FacebookLogo />} />
+				</View>
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Text style={{ color: 'grey', fontSize: s(14), marginRight: s(5) }}>
+						Create An Account
+					</Text>
+					<TouchableOpacity>
+						<Text
+							style={{
+								color: '#F83758',
+								fontSize: s(14),
+								textDecorationLine: 'underline',
+							}}
+						>
+							Sign Up
+						</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
 		</View>
 	);
 };
@@ -29,22 +76,27 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 		color: '#000',
 	},
-    forgotPasswordText: {
-        color: '#F83758',
-        fontSize: s(15),
-        marginTop: vs(9),
-        textAlign: 'right'
-    },
-    loginButton: {
-        marginTop: vs(40),
-        backgroundColor: '#F83758',
-        paddingVertical: vs(10),
-        borderRadius: s(4)
-    },
-    loginText: {
-        textAlign: 'center',
-        color: '#fff',
-        fontSize: s(20),
-        fontWeight: 'semibold'
-    }
+	forgotPasswordText: {
+		color: '#F83758',
+		fontSize: s(15),
+		marginTop: vs(9),
+		textAlign: 'right',
+	},
+	loginButton: {
+		marginTop: vs(40),
+		backgroundColor: '#F83758',
+		paddingVertical: vs(10),
+		borderRadius: s(4),
+	},
+	loginText: {
+		textAlign: 'center',
+		color: '#fff',
+		fontSize: s(20),
+		fontWeight: 'semibold',
+	},
+	socialContainer: {
+		marginTop: vs(75),
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
